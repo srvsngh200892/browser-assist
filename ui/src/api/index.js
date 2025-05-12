4b61c9958f9c9b1bb9c9f3a76bc994ff5e81907e
srvsngh200892 <saurav.singh@medable.com>
2025-05-13 00:22:50 +0400

    fix: move api to its folder

--- ui/src/api/index.js
+++ ui/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { ENDPOINTS } from './constants';
+import { ENDPOINTS } from '../constants';
 
 const api = axios.create({
   baseURL:  process.env.REACT_APP_SERVER_URL, // adjust as needed
